5b7f369415b9c1fbe4e489bb1533049ea08fc5d5
unknown <furkankara.9528@gmail.com>
2022-07-15 10:55:08 +0200

    Selenium Test

--- src/test/java/com/selenium/tests/jour6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/selenium/tests/jour6_alerts_iframes_windows/DropdownPractices.java
@@ -1,28 +1,68 @@
 package com.selenium.tests.jour6_alerts_iframes_windows;
 
 import com.selenium.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
 
-    @Test
-    public void dropdown_task5(){
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
         //TC #5: Selecting state from State dropdown and verifying result
         //1. Open Chrome browser
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        //2. Go to http://practice.cybertekschool.com/dropdown
-        driver.get("http://practice.cybertekschool.com/dropdown");
+        //2. Go to http://practice.cydeo.com/dropdown
+        driver.get("https://practice.cydeo.com/dropdown");
+    }
+
+
+    @Test
+    public void dropdown_task5() throws InterruptedException {
+
+
+
+        //we located the dropdwon and it is ready to use
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
 
         //3. Select Illinois
+        //Thread.sleep(2000);
+        stateDropdown.selectByVisibleText("Illinois");
+
         //4. Select Virginia
+        //Thread.sleep(2000);
+        stateDropdown.selectByValue("VA");
+
         //5. Select California
+        //Thread.sleep(2000);
+        stateDropdown.selectByIndex(5);
+
         //6. Verify final selected option is California.
+        String expectedOptionText = "California";
+        String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualOptionText, expectedOptionText);
         //Use all Select options. (visible text, value, index)
     }
+
+    @Test
+    public void dropdown_task(){
+
+
+    }
+
+
+
+
+
 }
