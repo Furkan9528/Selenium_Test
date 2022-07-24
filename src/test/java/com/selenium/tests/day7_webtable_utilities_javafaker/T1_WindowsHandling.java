6ba62daac86c61f26a6fa537574c3ed2cefc424c
unknown <furkankara.9528@gmail.com>
2022-07-24 19:42:39 +0200

    windows hangling task,

--- src/test/java/com/selenium/tests/day7_webtable_utilities_javafaker/T1_WindowsHandling.java
+++ src/test/java/com/selenium/tests/day7_webtable_utilities_javafaker/T1_WindowsHandling.java
@@ -1,18 +1,23 @@
 package com.selenium.tests.day7_webtable_utilities_javafaker;
 
+import com.selenium.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class T1_WindowsHandling {
 
     public WebDriver driver;
 
-
     @BeforeMethod
     public void setupMethod(){
 
-
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
@@ -20,12 +25,16 @@ public class T1_WindowsHandling {
     @Test
     public void window_handling_test(){
         //2. Go to : https://www.amazon.com
-        //3. Copy paste the lines from below into your class
+        driver.get("https://www.amazon.com");
+
+        //3. Copy-paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+
+
         //4. Create a logic to switch to the tab where Etsy.com is open
         //5. Assert: Title contains “Etsy”
-        // ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
-        // ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
-        // ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
 
     }
 
