4914ca6e491e43e490cb82b187ff90b51b07e603
unknown <furkankara.9528@gmail.com>
2022-07-24 20:09:39 +0200

    windows hangling task

--- src/test/java/com/selenium/tests/day7_webtable_utilities_javafaker/T1_WindowsHandling.java
+++ src/test/java/com/selenium/tests/day7_webtable_utilities_javafaker/T1_WindowsHandling.java
@@ -3,9 +3,11 @@ package com.selenium.tests.day7_webtable_utilities_javafaker;
 import com.selenium.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T1_WindowsHandling {
@@ -34,7 +36,28 @@ public class T1_WindowsHandling {
 
 
         //4. Create a logic to switch to the tab where Etsy.com is open
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for(String each : allWindowsHandles){
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if(driver.getCurrentUrl().contains("etsy")){
+                break;
+            }
+
+        }
+
         //5. Assert: Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+        String expectedInTitle = "Etsy";
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+        
 
     }
 
