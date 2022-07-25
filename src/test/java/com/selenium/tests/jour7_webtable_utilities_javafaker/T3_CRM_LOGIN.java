345805ebc587429915b74dd61820a39df6f70d8b
unknown <furkankara.9528@gmail.com>
2022-07-25 23:16:58 +0200

    T3_CRM_LOGIN

--- src/test/java/com/selenium/tests/jour7_webtable_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/selenium/tests/jour7_webtable_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,7 +1,10 @@
 package com.selenium.tests.jour7_webtable_utilities_javafaker;
 
+import com.selenium.utilities.BrowserUtils;
 import com.selenium.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -25,15 +28,24 @@ public class T3_CRM_LOGIN {
     public void crm_login_test(){
 
         //2. Go to : http://login1.nextbasecrm.com/
-        driver.get("http://login1.nextbasecrm.com/");
+        driver.get("https://login1.nextbasecrm.com/");
 
         //3. Enter valid username
-
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com") ;
 
         //4. Enter valid password
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
         //5. Click to `Log In` button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+
         //6. Verify title is as expected:
         //Expected: Portal
+        BrowserUtils.verifyTitle(driver, "Portal");
 
 
     }
