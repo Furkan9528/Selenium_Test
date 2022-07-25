a1f43c78cfaafcc0fec05693e6f3233d74130f39
unknown <furkankara.9528@gmail.com>
2022-07-25 22:38:13 +0200

    switchWindowsAndVerifiy method

--- src/test/java/com/selenium/utilities/BrowserUtils.java
+++ src/test/java/com/selenium/utilities/BrowserUtils.java
@@ -1,5 +1,15 @@
 package com.selenium.utilities;
 
+/*
+    In this class only general utility methods that are not related to some specific page.
+ */
+
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
 public class BrowserUtils {
 
     /*
@@ -7,16 +17,39 @@ public class BrowserUtils {
     for given duration
     */
 
-
-
-    public void sleep ( int second ){
-        second += 1000;
-
+    public static void sleep ( int second ){
+        second *= 1000;
         try{
             Thread.sleep(second);
         } catch (InterruptedException e) {
 
         }
     }
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle){
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for(String each : allWindowsHandles){
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if(driver.getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+
+        }
+
+
+        //5. Assert: Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+
+    }
+
 
 }
